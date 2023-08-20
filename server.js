af734f91587c1a23c26b9c34eb39c9e10ac1935f
Andrea Ceron <andreacmz1996@gmail.com>
2023-08-20 02:05:45 +0000

    add server

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const exphbs = require("express-handlebars");
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 1000 * 60 * 10,
+    expiration: 1000 * 60 * 30
+  })
+};
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +30,7 @@ const hbs = exphbs.create({ helpers });
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
